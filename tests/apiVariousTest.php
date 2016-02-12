7312193f72fea191be8d4955a29641ebde7309cb
Tjoosten <Tjoosten3@gmail.com>
2016-02-12 12:36:00 +0100

    add new testing methods

--- tests/apiVariousTest.php
+++ tests/apiVariousTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class apiVariousTest extends TestCase
 {
+    use DatabaseMigrations, DatabaseTransactions;
+
     /**
      * @group api
      */
