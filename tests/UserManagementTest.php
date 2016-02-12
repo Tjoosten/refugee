64d8b8b7bbfd499d9182c2de03d3ed6ae4e227c3
Tjoosten <Tjoosten3@gmail.com>
2016-02-12 12:36:59 +0100

    update

--- tests/UserManagementTest.php
+++ tests/UserManagementTest.php
@@ -6,36 +6,84 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UserManagementTest extends TestCase
 {
-    /**
-     * @group acl
-     *
-     * Test the redirect route if the user,
-     * Has the wrong permissions.
-     *
-     * 1 = block
-     * 0 = unblock
-     */
-    public function testBlockHandling()
-    {
-        $userInfo = factory(App\User::class)->create([
-            'status' => 0
-        ]);
+    use DatabaseMigrations, DatabaseTransactions;
 
-        $route = $this->actingAs($userInfo);
-        $route->visit('/acl/block/1/' . $userInfo->id);
-        $route->seeStatusCode(200);
+    /**
+     * UserManagementTest constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
     }
 
     /**
      * @group acl
      *
      * Test the redirect route if the user,
-     * Has the worng permission.
+     * - Has the wrong permissions.
      *
      * 1 = block
      * 0 = unblock
      */
-    public function testUnblockHandling()
+    public function testBlockHandlingIncorrectUser()
+    {
+        $user = factory(App\User::class)->create(['status' => 0]);
+
+        $route = $this->actingAs($user);
+        $route->seeInDatabase('users', ['id' => $user->id, 'status' => $user->status]);
+        $route->visit('/acl/block/1/' . $user->id);
+        $route->seeStatusCode(200);
+    }
+
+    /**
+     * @group acl
+     *
+     * Test the block feature off a user.
+     * - Has the correct permission.
+     *
+     * 1 = Block
+     * 0 = Unblock
+     */
+    public function testBlockHandlingCorrectUser()
+    {
+        $user = factory(App\User::class)->create();
+        Bouncer::assign('admin')->to($user);
+
+        $route = $this->actingAs($user);
+        $route->visit('/acl/block/1/' . $user->id);
+        $route->seeStatusCode(200);
+        $route->see('users', ['id' => $user->id, 'status' => 5]);
+    }
+
+    /**
+     * @group acl
+     *
+     * Test the redirect route if the user,
+     * - Has the wrong permission.
+     *
+     * 1 = block
+     * 0 = unblock
+     */
+    public function testUnblockHandlingIncorrectUser()
+    {
+        $user = factory(App\User::class)->create(['status' => 1]);
+
+        $route = $this->actingAs($user);
+        $route->visit('/acl/block/0/' . $user->id);
+        $route->seeInDatabase('users', ['id' => $user->id, 'status' => $user->status]);
+        $route->seeStatusCode(200);
+    }
+
+    /**
+     * @group acl
+     *
+     * Test if the user can unblock a user.
+     * - Has the incorrect permission.
+     *
+     * 1 = block
+     * 0 = unblock
+     */
+    public function testUnblockHandlingCorrectUser()
     {
 
     }
