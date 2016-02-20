ebedb0669f4a171f864714a2142be233153ebc0c
Tjoosten <Tjoosten3@gmail.com>
2016-02-20 13:24:29 +0100

    SFA - Add migration for the collecting points

---
+++ database/migrations/2016_02_18_194524_PointsTable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inzamelpunten', function(Blueprint $t) {
+            $t->increments('id');
+            $t->string('Opening_maandag');
+            $t->string('Opening_dinsdag');
+            $t->string('Opening_woensdag');
+            $t->string('Opening_donderdag');
+            $t->string('Opening_vrijdag');
+            $t->string('adress');
+            $t->string('contact');
+            $t->string('naam_inzamelpunt');
+            $t->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfexists('inzamelpunten');
+    }
+}
