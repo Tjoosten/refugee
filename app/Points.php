a29dcc00bf54313f577bd1dc4837193cbc062808
Tjoosten <Tjoosten3@gmail.com>
2016-02-20 14:37:07 +0100

    SFA - Add database column property's to the model.

--- app/Points.php
+++ app/Points.php
@@ -4,6 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string Opening_dinsdag
+ * @property string Opening_maandag
+ * @property string Opening_woensdag
+ * @property string Opening_donderdag
+ * @property string contact
+ * @property string address
+ * @property string naam_inzamelpunt
+ * @property string Opening_vrijdag
+ * @property string Opening_zaterdag
+ * @property mixed|string Opening_zondag
+ */
 class Points extends Model
 {
     /**
