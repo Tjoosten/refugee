388572a5f701e7c104877cbef06ad67e6bc0a93c
Tjoosten <Tjoosten3@gmail.com>
2016-02-20 13:20:56 +0100

    Add the model for the collection points

---
+++ app/Points.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Points extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'inzamelpunten';
+}
