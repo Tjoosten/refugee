7fa2c22503a55b8fe1539b044dc7848e6a205e5f
Tjoosten <Tjoosten3@gmail.com>
2016-02-25 16:06:41 +0100

    add github validator request

--- app/Http/Controllers/bugController.php
+++ app/Http/Controllers/bugController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\githubValidator;
 use Github\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
+/**
+ * Class bugController
+ * @package App\Http\Controllers
+ */
 class bugController extends Controller
 {
     /**
@@ -37,29 +42,22 @@ class bugController extends Controller
      * And store the user email privately.
      * To add later the function to email the user if the bug is closed.
      *
-     * @param Request $input
+     * @param githubValidator $input
      */
-    public function send(Request $input)
+    public function send(githubValidator $input)
     {
         $client = new Client();
         $client->authenticate('Tjoosten', '0474834880Tim!', $client::AUTH_HTTP_PASSWORD);
 
-        if (isset($input->body) || isset($input->title)) {
-            $client->api('issue')->create('Tjoosten', 'Refugee', [
-                'title' => $input->title,
-                'body'  => $input->body,
-            ]);
+        $client->api('issue')->create('Tjoosten', 'Refugee', [
+            'title' => $input->title,
+            'body'  => $input->body,
+        ]);
 
-            // Success: Error created.
-            session()->flash('flash_title', 'Bedankt.');
-            session()->flash('flash_message', 'Wij hebben je melding geregistreerd.');
-            session()->flash('flash_message_important', true);
-        } else {
-            // Error: Issue not created
-            session()->flash('flash_title', 'Oh Snapp :(');
-            session()->flash('flash_message', 'Wij konden je melding niet registreren.');
-            session()->flash('flash_message_important', true);
-        }
+        // Success: Error created.
+        session()->flash('flash_title', 'Bedankt.');
+        session()->flash('flash_message', 'Wij hebben je melding geregistreerd.');
+        session()->flash('flash_message_important', true);
 
         return Redirect::back();
     }
